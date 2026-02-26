6711852d8500a111f8d6934360a8d02430bb9a9b
Mikhail Golyachkov <cp7reg@gmail.com>
2026-02-26 20:03:09 +0300

    Add pgxpool and middleware pgx tx

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,7 +9,9 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/Nublnv/go-service/cmd/internal/middleware"
 	router "github.com/Nublnv/go-service/cmd/internal/router"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -29,9 +31,37 @@ func main() {
 		panic("TLS_CERT and TLS_KEY env variables must be set both or not set at all")
 	}
 
+	dbHost := os.Getenv("POSTGRES_HOST")
+	if dbHost == "" {
+		panic("POSTGRES_HOST env variable is not set")
+	}
+	dbPort := os.Getenv("POSTGRES_PORT")
+	if dbPort == "" {
+		dbPort = "5432"
+	}
+	dbUser := os.Getenv("POSTGRES_USER")
+	if dbUser == "" {
+		panic("POSTGRES_USER env variable is not set")
+	}
+	dbPass := os.Getenv("POSTGRES_PASS")
+	if dbPass == "" {
+		panic("POSTGRES_PASS env variable is not set")
+	}
+	dbName := os.Getenv("POSTGRES_DB")
+	if dbName == "" {
+		panic("POSTGRES_DB env variable is not set")
+	}
+
+	baseContext := context.Background()
+
+	pool, err := pgxpool.New(baseContext, fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName))
+	if err != nil {
+		panic("Failed to connect postgres db with provided credentials")
+	}
+
 	svc := &http.Server{
 		Addr:    fmt.Sprintf("%s:%s", host, port),
-		Handler: router.GetRouter(),
+		Handler: middleware.DbMiddleware(pool)(router.GetRouter()),
 	}
 
 	if tlsCert != "" && tlsKey != "" {
@@ -55,7 +85,8 @@ func main() {
 	<-c
 	fmt.Println("Shutting down server...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(baseContext, 5*time.Second)
+	defer pool.Close()
 	defer cancel()
 
 	if err := svc.Shutdown(ctx); err != nil {
--- cmd/internal/middleware/middleware.go
+++ cmd/internal/middleware/middleware.go
@@ -1,10 +1,12 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/Nublnv/go-service/cmd/internal/errorHandler"
 	"github.com/Nublnv/go-service/cmd/internal/errors"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func AuthMiddleware(next http.Handler) http.Handler {
@@ -18,4 +20,30 @@ func AuthMiddleware(next http.Handler) http.Handler {
 			return errors.Unauthorized(1001, "Unauthorized", nil)
 		}
 	}))
+}
+
+func DbMiddleware(pool *pgxpool.Pool) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(errorHandler.Wrap(func(w http.ResponseWriter, r *http.Request) error {
+			conn, err := pool.Acquire(r.Context())
+			if err != nil {
+				return errors.InternalServerError(500, "DB unavailible", nil)
+			}
+			defer conn.Release()
+
+			tx, err := conn.Begin(r.Context())
+			if err != nil {
+				return errors.InternalServerError(500, "Tx begin failed", nil)
+			}
+			defer tx.Rollback(r.Context())
+
+			ctx := context.WithValue(r.Context(), "tx", tx)
+			next.ServeHTTP(w, r.WithContext(ctx))
+
+			if err := tx.Commit(r.Context()); err != nil {
+				tx.Rollback(r.Context())
+			}
+			return nil
+		}))
+	}
 }
