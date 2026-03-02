fdd281afbd6cf0a0e7d7731dfe2e0c545891891e
Mikhail Golyachkov <cp7reg@gmail.com>
2026-03-02 20:04:00 +0300

    Add login method

--- cmd/internal/public/public.go
+++ cmd/internal/public/public.go
@@ -2,11 +2,13 @@ package public
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/Nublnv/go-service/cmd/internal/errorHandler"
 	"github.com/Nublnv/go-service/cmd/internal/errors"
+	"github.com/Nublnv/go-service/cmd/internal/middleware"
 	"github.com/jackc/pgx/v5"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -29,6 +31,11 @@ func GetPublicHandler() http.Handler {
 type RegisterRequest struct {
 	Username string `json:"login"`
 	Password string `json:"password"`
+}
+
+type UserData struct {
+	login    string
+	passhash []byte
 }
 
 func register(w http.ResponseWriter, r *http.Request) error {
@@ -75,6 +82,45 @@ func register(w http.ResponseWriter, r *http.Request) error {
 }
 
 func login(w http.ResponseWriter, r *http.Request) error {
-	// TODO - implement login logic here
+	if r.Method != http.MethodPost {
+		errors.MethodNotAllowed(1000, "Method not allowed", nil, r)
+	}
+
+	if err := r.ParseForm(); err != nil {
+		return errors.BadRequest(1002, "Invalid form data", err, r)
+	}
+
+	loginReq := &RegisterRequest{
+		Username: r.FormValue("login"),
+		Password: r.FormValue("password"),
+	}
+
+	var db pgx.Tx = r.Context().Value("db").(pgx.Tx)
+
+	userData := &UserData{}
+
+	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+	defer cancel()
+
+	res := db.QueryRow(ctx, "SELECT login, passhash FROM auth.users WHERE login = $1", loginReq.Username)
+	err := res.Scan(&userData.login, &userData.passhash)
+	if err != nil && err != pgx.ErrNoRows {
+		return errors.BadRequest(500, "Something went wrong", err, r)
+	} else if err == pgx.ErrNoRows {
+		return errors.BadRequest(400, "User not found", nil, r)
+	}
+
+	err = bcrypt.CompareHashAndPassword(userData.passhash, []byte(loginReq.Password))
+	if err != nil {
+		return errors.Forbidden(403, "Wrong password", err, r)
+	}
+
+	token, err := middleware.GetToket(userData.login)
+	if err != nil {
+		return errors.InternalServerError(500, "Internal server error", err, r)
+	}
+
+	w.Header().Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
 	return nil
 }
