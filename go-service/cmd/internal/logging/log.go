83bd0f9d8ee237d354a99bb6c0374e47437e5e49
Mikhail Golyachkov <cp7reg@gmail.com>
2026-03-06 20:25:12 +0300

    Do logging in separate goroutine

--- go-service/cmd/internal/logging/log.go
+++ go-service/cmd/internal/logging/log.go
@@ -6,33 +6,54 @@ import (
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func LogUserAction(ctx context.Context, ch clickhouse.Conn, pg pgx.Tx, action_type string, user_id int) {
+type ActionData struct {
+	actionType string
+	userid     int
+}
 
-	var action_id int
-	err := pg.QueryRow(ctx, "SELECT id FROM dicts.actions WHERE label = $1", action_type).Scan(&action_id)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+func LogUserAction(ctx context.Context, actionType string, userid int) {
+	var c chan<- *ActionData = ctx.Value("logging").(chan *ActionData)
 
-	query := "INSERT INTO user_actions (dt, user_id, action_id) VALUES ($1, $2, $3)"
+	c <- &ActionData{
+		actionType: actionType,
+		userid:     userid,
+	}
+}
 
-	batch, err := ch.PrepareBatch(ctx, query)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	err = batch.Append(time.Now(), user_id, action_id)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	err = batch.Send()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+func DoLoggingActions(ctx context.Context, ch clickhouse.Conn, pg *pgxpool.Conn) {
+	var c <-chan *ActionData = ctx.Value("logging").(chan *ActionData)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case data := <-c:
+			var action_id int
+			err := pg.QueryRow(ctx, "SELECT id FROM dicts.actions WHERE label = $1", data.actionType).Scan(&action_id)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			query := "INSERT INTO user_actions (dt, user_id, action_id) VALUES ($1, $2, $3)"
+
+			batch, err := ch.PrepareBatch(ctx, query)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			err = batch.Append(time.Now(), data.userid, action_id)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			err = batch.Send()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+		}
 	}
 }
